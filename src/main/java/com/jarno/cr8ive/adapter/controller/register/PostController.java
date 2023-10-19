6a58e67a7e9f703a1225d00ae9586045836a8b92
Jarno Dijkmans <513745@student.fontys.nl>
2023-10-19 18:09:26 +0200

    Used new clean architecture

--- src/main/java/com/jarno/cr8ive/adapter/controller/register/PostController.java
+++ src/main/java/com/jarno/cr8ive/adapter/controller/register/PostController.java
@@ -5,12 +5,16 @@ import com.jarno.cr8ive.business.exeption.PostCustomException;
 import com.jarno.cr8ive.business.model.request.CreatePostRequestModel;
 import com.jarno.cr8ive.business.model.response.CreatePostResponseModel;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
+
+@RestController
+@RequestMapping("/posts")
 @AllArgsConstructor
+@CrossOrigin(origins = "http://localhost:5173")
 public class PostController {
     IPostRegisterBoundary inputBoundary;
+
 
     @PostMapping
     public CreatePostResponseModel create (@RequestBody CreatePostRequestModel requestModel) throws PostCustomException {
